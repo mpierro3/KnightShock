32c7b7cab603c2cbeaef0409b0460a331745d1b8
Cory Kinney <corykinney@knights.ucf.edu>
2022-10-11 17:09:32 -0400

    Update absorption.py
    Add `absorbance` and `absorption_cross_section` functions to complement existing `species_mole_fraction` function

--- knightshock/absorption.py
+++ knightshock/absorption.py
@@ -1,7 +1,3 @@
-"""
-Absorption spectroscopy module
-"""
-
 import numpy.typing as npt
 
 
@@ -12,6 +8,53 @@ GAS_CONSTANT = 8.31432e3
 """Universal gas constant [J mol^-1^ K^-1^]."""
 
 
+def absorbance(
+        X: float | npt.NDArray[float],
+        sigma: float | npt.NDArray[float],
+        T: float | npt.NDArray[float],
+        P: float | npt.NDArray[float],
+        L: float | npt.NDArray[float]
+) -> float | npt.NDArray[float]:
+    r"""Calculates the absorption for a species from the Beer-Lambert law.
+
+    Args:
+        X: Species mole fraction.
+        sigma: Absorption cross-section [cm^2^].
+        T: Absolute temperature [K].
+        P: Absolute pressure [Pa].
+        L: Path length [cm].
+
+    Returns:
+        A: Absorbance.
+
+    """
+    return sigma * X * AVOGADRO_NUMBER * L * P / (GAS_CONSTANT * T) / 1E6
+
+
+def absorption_cross_section(
+        A: float | npt.NDArray[float],
+        X: float | npt.NDArray[float],
+        T: float | npt.NDArray[float],
+        P: float | npt.NDArray[float],
+        L: float | npt.NDArray[float]
+) -> float | npt.NDArray[float]:
+    """Calculates the species mole fraction from the Beer-Lambert law.
+
+    Args:
+        A: Absorbance.
+        X: Species mole fraction.
+        T: Absolute temperature [K].
+        P: Absolute pressure [Pa].
+        L: Path length [cm].
+
+    Returns:
+        sigma: Absorption cross-section [cm^2^].
+
+    """
+
+    return A / (X * AVOGADRO_NUMBER * L * P / (GAS_CONSTANT * T)) * 1E6
+
+
 def species_mole_fraction(
         A: float | npt.NDArray[float],
         sigma: float | npt.NDArray[float],
@@ -19,31 +62,18 @@ def species_mole_fraction(
         P: float | npt.NDArray[float],
         L: float | npt.NDArray[float]
 ) -> float | npt.NDArray[float]:
-    r"""
-    Calculates the species mole fraction according to the Beer-Lambert law
+    """Calculates the species mole fraction from the Beer-Lambert law.
 
-    $$
-    A = \sigma X N_A L \frac{P}{RT}
-    $$
+    Args:
+        A: Absorbance.
+        sigma: Absorption cross-section [cm^2^].
+        T: Absolute temperature [K].
+        P: Absolute pressure [Pa].
+        L: Path length [cm].
 
-    Parameters
-    ----------
-    A:
-        Absorbance.
-    sigma:
-        Absorption cross-section [cm^2^].
-    T:
-        Absolute temperature [K].
-    P:
-        Absolute pressure [Pa].
-    L:
-        Path length [cm].
-
-    Returns
-    -------
-    X:
-        Species mole fraction.
+    Returns:
+        X: Species mole fraction.
 
     """
 
-    return A / (sigma / 1E6 * AVOGADRO_NUMBER * L) * (GAS_CONSTANT * T) / P
+    return A / (sigma * AVOGADRO_NUMBER * L * P / (GAS_CONSTANT * T)) * 1E6
