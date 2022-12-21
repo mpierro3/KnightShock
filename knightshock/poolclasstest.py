4901fd3b44b2b08ae28570a79d6ff41ec158e1f1
Christopher Dennis <78862152+ChrisWD24@users.noreply.github.com>
2022-12-21 15:59:42 -0500

    Update poolclasstest.py
    fixed Xhistory fragmentation error and added IDT csv output

--- knightshock/poolclasstest.py
+++ knightshock/poolclasstest.py
@@ -5,18 +5,25 @@ import itertools
 from time import time
 from kinetics import Simulation
 import pandas as pd
+import csv
+import os
 
 
 class SimulationPool:
 
-    def __init__(self, Mechs: list, Mixtures: dict, Pressures: np.array, Temps: np.array, *, nProcs=None):
+    def __init__(self, Mechs: list, Mixtures: dict, Pressures: np.array, Temps: np.array, *,IDT_species=None, nProcs=None):
 
         self.Mechs = Mechs
         self.gases = {}
         self.Mixtures = Mixtures
         self.initialPressures = Pressures
         self.intialTemps = Temps
-        self.parallel()
+        self.IDT_species =IDT_species
+
+        for mix in self.Mixtures.keys():
+            with open(f'{mix} IDT.csv', 'w',newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(['mechanism','Pressure(Pa)', 'Temperature(K)', 'IDT(s)'])
 
     def parallel(self):
         """
@@ -58,11 +65,18 @@ class SimulationPool:
         """function for saving simulation data to csv"""
         mech, X, P, T = initialconditions
         mix_name = self.get_key(self.Mixtures, X)
-        print(f'{mech}_P={P / 1e6:.2}MPa_T={T}K_{mix_name}')
+        IDT =sim.ignition_delay_time(self.IDT_species)
+        print(f'saving {str(sim.gas.source).split(".")[0]}_P={P / 1e6:.2}MPa_T={T}K_{mix_name}')
+        'Saving species time history data'
         Xhistory = pd.DataFrame()
         for species in sim.get_top_species():
-            Xhistory[species] = sim.X(species)
-        Xhistory.to_csv(f'{mech}_P={P / 1e6:.2}MPa_T={T}K_{mix_name}.csv')
+            species_history = pd.DataFrame(sim.X(species), columns=[species])
+            Xhistory = pd.concat([Xhistory, species_history], axis=1)
+        Xhistory.to_csv(f'{os.path.basename(sim.gas.source)}_P={P / 1e6:.2}MPa_T={T}K_{mix_name}.csv')
+        'Saving IDT data'
+        with open(f'{mix_name} IDT.csv', 'a',newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([sim.gas.source,P,T,IDT])
 
     def get_key(self, dictionary, val):
         for key, value in dictionary.items():
@@ -70,13 +84,29 @@ class SimulationPool:
                 return key
         return "key doesn't exist"
 
+    def serial(self):
+        tupled_inputs = list(itertools.product(self.Mechs, self.Mixtures.values(),
+                                               self.initialPressures, self.intialTemps))
+
+        y = list(map(self.run, tupled_inputs))
+        return y
+
 
 if __name__ == '__main__':
     T = np.array([1300])
     P = np.array([1e6])
     mixtures = {}
-    mixtures['mix A'] = 'CH4: 0.04, O2: 0.08, AR: 0.88'
-    mixtures['mix B'] = 'CH4: 0.05, O2: 0.08, AR: 0.88'
-    Mechanisms = ['aramco3.yaml']
 
-    SimulationPool(Mechanisms, mixtures, P, T)
+    mixtures['mix A'] = 'CH4: 0.04, O2: 0.08, AR: 0.88'
+    mixtures['mix B'] = 'CH4: 0.06, O2: 0.08, AR: 0.86'
+    Mechanisms = [r'C:\Users\ch733787\Documents\GitHub\KnightShock\HPST\gri30.yaml']
+
+    t1 = time()
+    SimulationPool(Mechanisms, mixtures, P, T,IDT_species='OH').parallel()
+    t2 = time()
+    print('Parallel: {0:.3f} seconds'.format(t2 - t1))
+
+    # t1 = time()
+    # SimulationPool(Mechanisms, mixtures, P, T).serial()
+    # t2 = time()
+    # print('Serial: {0:.3f} seconds'.format(t2 - t1))
